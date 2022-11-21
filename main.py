e5ea1bdaf9ee6276a309388ff8263c092f26f270
vanish <118196151+cumsack@users.noreply.github.com>
2022-11-21 15:33:52 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# ez bypass lolz
+
+import random
+
+email = input("Email: ")
+username = email.split("@")[0]
+domain=email.split("@")[1]
+
+number = 0
+while True:
+  number =+ 1
+  open("emails.txt", "a").write(username + "+" + str(number) + "@" + domain)
+  print(username + "+" + str(number) + "@" + domain)
